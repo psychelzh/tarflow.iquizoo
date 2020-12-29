0d48193210dff334b7363d89a1df451cc842aac6
Liang Zhang <psychelzh@outlook.com>
2020-12-29 21:37:35 +0800

    🎈perf(data): simplify data
    The `game_name_abbr` variable is no longer required in config now.
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- data-raw/config.R
+++ data-raw/config.R
@@ -1,8 +1,6 @@
-## code to prepare `gamenames`(exported) and `game_config`(internal) dataset goes here
+## code to prepare `gameinfo`(exported) and `gameconfig`(internal) datasets
 config <- readr::read_csv("data-raw/config.csv", col_types = readr::cols())
-gameinfo <- config %>%
-  dplyr::select(game_id, dplyr::starts_with("game_name"))
-gameconfig <- config %>%
-  dplyr::select(game_id, game_name, game_name_abbr, prep_fun)
+gameinfo <- dplyr::select(config, game_id, dplyr::starts_with("game_name"))
+gameconfig <- dplyr::select(config, game_id, game_name, prep_fun)
 usethis::use_data(gameinfo, overwrite = TRUE)
 usethis::use_data(gameconfig, overwrite = TRUE, internal = TRUE)
