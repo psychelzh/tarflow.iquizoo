9b619650fbf84f18ff8e1ca9f2c7c61ec9fba1fc
Liang Zhang <psychelzh@outlook.com>
2021-02-27 23:20:03 +0800

    🎈perf(utils): use s3 method
    Now more types of `config_where` is supported.
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/fetch_from_v3.R
+++ R/fetch_from_v3.R
@@ -5,7 +5,8 @@
 #' `where_config` is `NULL`, this will extract all data.
 #'
 #' @param query_file File name of `sql` query
-#' @param config_where A `list` storing configuration of `where-clause`
+#' @param config_where Configuration of "where-clause" of the `sql` query. Can
+#'   be a `list` (mostly from the `config.yml` file) or `data.frame`.
 #' @return A `tibble` of original data
 #' @author Liang Zhang
 #' @importFrom rlang .data
@@ -24,34 +25,45 @@ fetch_from_v3 <- function(query_file, config_where = NULL) {
 }
 
 compose_where_clause <- function(config_where) {
-  if (is.null(config_where)) {
-    return("")
-  } else {
-    config_where_tbl <- tibble::tibble(where = config_where) %>%
-      tidyr::unnest_wider("where")
-    if (!rlang::has_name(config_where_tbl, "operator")) {
-      config_where_tbl$operator <- NA_character_
-    }
-    where_base <- config_where_tbl %>%
-      dplyr::mutate(
-        operator = dplyr::case_when(
-          is.na(.data$operator) & lengths(.data$values) == 1 ~ "=",
-          is.na(.data$operator) & lengths(.data$values) > 1 ~ "IN",
-          TRUE ~ .data$operator
-        ),
-        value_str = purrr::map_chr(
-          .data$values,
-          ~ if (length(.x) == 1) {
-            stringr::str_c("'", .x, "'")
-          } else {
-            stringr::str_c(
-              "(", stringr::str_c("'", .x, "'", collapse = ", "), ")"
-            )
-          }
-        )
-      ) %>%
-      stringr::str_glue_data("{table}.{field} {operator} {value_str}") %>%
-      stringr::str_c(collapse = " AND ")
-    stringr::str_c("WHERE", where_base, sep = " ")
+  UseMethod("compose_where_clause")
+}
+compose_where_clause.default <- function(config_where) {
+  config_where
+}
+compose_where_clause.NULL <- function(config_where) {
+  compose_where_clause.default("")
+}
+compose_where_clause.character <- function(config_where) {
+  compose_where_clause.default(config_where)
+}
+compose_where_clause.list <- function(config_where) {
+  config_where_tbl <- tibble::tibble(where = config_where) %>%
+    tidyr::unnest_wider("where")
+  compose_where_clause(config_where_tbl)
+}
+compose_where_clause.data.frame <- function(config_where) {
+  if (!rlang::has_name(config_where, "operator")) {
+    config_where$operator <- NA_character_
   }
+  where_base <- config_where %>%
+    dplyr::mutate(
+      operator = dplyr::case_when(
+        is.na(.data$operator) & lengths(.data$values) == 1 ~ "=",
+        is.na(.data$operator) & lengths(.data$values) > 1 ~ "IN",
+        TRUE ~ .data$operator
+      ),
+      value_str = purrr::map_chr(
+        .data$values,
+        ~ if (length(.x) == 1) {
+          stringr::str_c("'", .x, "'")
+        } else {
+          stringr::str_c(
+            "(", stringr::str_c("'", .x, "'", collapse = ", "), ")"
+          )
+        }
+      )
+    ) %>%
+    stringr::str_glue_data("{table}.{field} {operator} {value_str}") %>%
+    stringr::str_c(collapse = " AND ")
+  compose_where_clause.default(stringr::str_c("WHERE", where_base, sep = " "))
 }
