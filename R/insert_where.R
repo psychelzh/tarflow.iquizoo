7edad72e8132a6727bc53fc180f5f920d7f2d070
Liang Zhang <psychelzh@outlook.com>
2021-03-20 23:11:56 +0800

    ✨feat(general): support multi-insert
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/insert_where.R
+++ R/insert_where.R
@@ -3,14 +3,18 @@
 #' Insert new where configurations into the existed one. This can be useful in
 #' many projects doing minor changes to current where configurations.
 #'
-#' This function is intended to modify an existed where configuration. To create
-#' a completely new one, please specify it by using [tibble::tribble()] instead.
+#' @details This function is intended to modify an existed where configuration.
+#'   To create a completely new one, please specify it by using
+#'   [tibble::tribble()] instead.
+#'
+#'   If you insisted on calling [insert_where()] with empty where configration,
+#'   it will signal an error.
 #'
 #' @param old The old where configuration.
-#' @param element The new where element. Can be a `list` or `data.frame`. `NULL`
-#'   is accepted so that returns an unchanged version of the old config.
-#' @param ... Other argument kept for future use. Currently, they are silently
-#'   ignored.
+#' @param ... The new where element. Can be of `list` or `data.frame` class.
+#'   Note they must have name of `"table"`. What's more, to make it work
+#'   properly in the whole project, the names should be no other than
+#'   `c("table", "field", "operator", "values")`.
 #' @param replace Replace the element with the same `table` value or not.
 #'   Default is set to replace, set `FALSE` to add it anyway (not recommended,
 #'   might cause error in further analysis).
@@ -25,30 +29,31 @@ insert_where <- function(old, ...) {
 #' @rdname insert_where
 #' @exportS3Method insert_where
 insert_where.NULL <- function(old, ...) {
-  NULL
+  stop("You are trying to create new where configuration.",
+       "Please use `tibble::tribble()` instead.")
 }
 
 #' @rdname insert_where
 #' @exportS3Method insert_where
-insert_where.list <- function(old, element, ..., replace = TRUE) {
-  if (is.null(element)) {
-    return(old)
+insert_where.list <- function(old, ..., replace = TRUE) {
+  if (length(old) == 0) insert_where.NULL(old)
+  new <- purrr::map(list(...), ~ as.list(unlist(.x)))
+  if (!all(purrr::map_lgl(new, ~ rlang::has_name(.x, "table")))) {
+    stop("At least one of the new element has no name of 'table'.")
   }
-  element <- as.list(unlist(element))
+  new_tables <- purrr::map_chr(new, ~ .x[["table"]])
   if (replace) {
-    old[purrr::map_lgl(old, ~ .x[["table"]] == element[["table"]])] <- NULL
+    old[purrr::map_lgl(old, ~ .x[["table"]] %in% new_tables)] <- NULL
   }
-  return(c(old, list(element)))
+  return(c(old, new))
 }
 
 #' @rdname insert_where
 #' @exportS3Method insert_where
-insert_where.data.frame <- function(old, element, ..., replace = TRUE) {
-  if (is.null(element)) {
-    return(old)
-  }
+insert_where.data.frame <- function(old, ..., replace = TRUE) {
+  if (nrow(old) == 0) insert_where.NULL(old)
   old <- as.list(old) %>% purrr::transpose()
-  insert_where(old, element, ..., replace = replace) %>%
+  insert_where(old, ..., replace = replace) %>%
     purrr::transpose() %>%
     tibble::as_tibble() %>%
     tidyr::unnest(-.data[["values"]])
