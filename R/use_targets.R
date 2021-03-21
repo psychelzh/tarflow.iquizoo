7169cfffaca7f9045a119a39a1d11fa2b0799a33
Liang Zhang <psychelzh@outlook.com>
2021-03-21 11:43:42 +0800

    ✨feat(general): support separate
    This feature is currently very important when the original data table becomes larger and larger.
    
    Fix #10
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/use_targets.R
+++ R/use_targets.R
@@ -6,53 +6,61 @@
 #'   from rawdata, mostly for scholar usage; 'scores' means extracting the
 #'   scores directly from online database, mostly for norm calculation and
 #'   report generation; 'original' fetches raw data only.
+#' @param separate Separate main targets into branches or not. For `"indices"`
+#'   and `"original"` schema, the default is set to `TRUE`. For `"scores"`
+#'   schema, the default is set to `FALSE`.
 #' @author Liang Zhang
 #' @export
-use_targets <- function(schema = c("indices", "scores", "original")) {
+use_targets <- function(schema = c("indices", "scores", "original"),
+                        separate = NULL) {
   schema <- match.arg(schema)
-  if ("indices" == schema) {
-    # add required query files
-    usethis::use_directory("sql")
-    usethis::use_template("sql/data.tmpl.sql", package = "tarflow.iquizoo")
-    usethis::use_template("sql/users.tmpl.sql", package = "tarflow.iquizoo")
-    usethis::ui_done("Added query files for raw data and users fetching.")
-    # add configuration files
-    usethis::use_template("config.yml", package = "tarflow.iquizoo")
-    usethis::ui_done("Added basic config file.")
-    usethis::ui_todo("You probably need to edit the config file {usethis::ui_value('config.yml')}.")
-    # add targets file
-    usethis::use_template("_targets_indices.R", save_as = "_targets.R", package = "tarflow.iquizoo")
-    # ignore targets internal files
-    usethis::use_git_ignore("_targets")
+  # set `separate` default to FALSE for a schema of "scores"
+  if (is.null(separate)) {
+    separate <- ifelse(schema == "scores", FALSE, TRUE)
   }
-  if ("scores" == schema) {
-    # add required query files
-    usethis::use_directory("sql")
-    usethis::use_template("sql/scores.tmpl.sql", package = "tarflow.iquizoo")
-    usethis::use_template("sql/users.tmpl.sql", package = "tarflow.iquizoo")
-    usethis::ui_done("Added query files for scores and users fetching.")
-    # add configuration files
-    usethis::use_template("config.yml", package = "tarflow.iquizoo")
-    usethis::ui_done("Added basic config file.")
-    usethis::ui_todo("You probably need to edit the config file {usethis::ui_value('config.yml')}.")
-    # add targets file
-    usethis::use_template("_targets_scores.R", save_as = "_targets.R", package = "tarflow.iquizoo")
-    # ignore targets internal files
-    usethis::use_git_ignore("_targets")
-  }
-  if ("original" == schema) {
-    # add required query files
-    usethis::use_directory("sql")
-    usethis::use_template("sql/data.tmpl.sql", package = "tarflow.iquizoo")
-    usethis::use_template("sql/users.tmpl.sql", package = "tarflow.iquizoo")
-    usethis::ui_done("Added query files for raw data and users fetching.")
-    # add configuration files
-    usethis::use_template("config.yml", package = "tarflow.iquizoo")
-    usethis::ui_done("Added basic config file.")
-    usethis::ui_todo("You probably need to edit the config file {usethis::ui_value('config.yml')}.")
-    # add targets file
-    usethis::use_template("_targets_original.R", save_as = "_targets.R", package = "tarflow.iquizoo")
-    # ignore targets internal files
-    usethis::use_git_ignore("_targets")
-  }
+  stopifnot(rlang::is_scalar_logical(separate))
+  # prepare names of templates to be used
+  templates_query <- c(
+    "sql/users.tmpl.sql",
+    switch(
+      schema,
+      original = ,
+      indices = "sql/data.tmpl.sql",
+      scores = "sql/scores.tmpl.sql"
+    ),
+    if (separate) "sql/games.tmpl.sql"
+  )
+  template_targets <- file.path(
+    "schemas", schema,
+    ifelse(separate, file.path("separate", "_targets.R"), "_targets.R")
+  )
+  # trigger side effects
+  # add required query files
+  usethis::use_directory("sql")
+  purrr::walk(
+    templates_query,
+    ~ usethis::use_template(.x, package = "tarflow.iquizoo")
+  )
+  usethis::ui_done("Added query files for fetching datasets.")
+  # add configuration files
+  usethis::use_template("config.yml", package = "tarflow.iquizoo")
+  usethis::ui_done("Added config file template.")
+  usethis::ui_todo(
+    paste("You probably need to edit the config file",
+          "{usethis::ui_value('config.yml')}.")
+  )
+  # add targets file
+  usethis::use_template(
+    template_targets,
+    save_as = "_targets.R",
+    package = "tarflow.iquizoo"
+  )
+  usethis::ui_done("Added {usethis::ui_value('_targets.R')}.")
+  # ignore targets internal files
+  usethis::use_git_ignore("_targets")
+  usethis::ui_done(
+    paste("All work done.",
+          "Please run {usethis::ui_code('targets::tar_make()')}",
+          "after editting {usethis::ui_value('config.yml')}.")
+  )
 }
