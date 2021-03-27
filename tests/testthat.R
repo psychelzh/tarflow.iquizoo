2c0a77f1941684c46d2a5fe5f37902bbc1236af8
Liang Zhang <psychelzh@outlook.com>
2021-03-27 17:24:45 +0800

    🐳chore(test): add unit test
    The major test tool is `expect_snapshot_file()`. See details at `?expect_snapshot_file` in R.
    
    fix #3
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(tarflow.iquizoo)
+
+test_check("tarflow.iquizoo")
---
+++ tests/testthat/_snaps/use_targets/original-no-sep_users.tmpl.sql
@@ -0,0 +1,25 @@
+-- !preview conn=DBI::dbConnect(odbc::odbc(), "iquizoo-v3", database = "iquizoo_datacenter_db")
+
+SELECT DISTINCT
+	`user`.Id user_id,
+	`user`.RealName user_name,
+	`user`.Gender user_sex,
+	`user`.Birthday user_dob,
+	base_organization.`Name` school,
+	base_organization.Province province,
+	base_organization.City city,
+	base_organization.District district,
+	base_grade_class.GradeName grade,
+	base_grade_class.ClassName class
+FROM
+	iquizoo_user_db.`user`
+	INNER JOIN iquizoo_user_db.user_organization ON user_organization.UserId = `user`.Id
+	INNER JOIN iquizoo_user_db.base_organization ON base_organization.Id = user_organization.OrganizationId  -- `base_organization` might be used in "where_clause"
+	INNER JOIN iquizoo_user_db.base_grade_class ON base_grade_class.Id = user_organization.ClassId
+	INNER JOIN iquizoo_content_db.project_course_user ON project_course_user.UserId = user_organization.Id
+	INNER JOIN iquizoo_content_db.course ON course.Id = project_course_user.CourseId -- `course` might be used in "where_clause"
+	INNER JOIN iquizoo_content_db.course_child ON course_child.CourseId = course.Id
+	INNER JOIN iquizoo_content_db.course_child_config ON course_child_config.ChildCourseId = course_child.Id
+	INNER JOIN iquizoo_content_db.content ON content.Id = course_child_config.ContentId -- `content` might be used in "where_clause"
+	INNER JOIN iquizoo_content_db.projects ON projects.Id = project_course_user.ProjectId -- -- `projects` might be used in "where_clause"
+{ where_clause };
