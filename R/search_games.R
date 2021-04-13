e68fd8f935757434fb036957360cafbfce4704ca
Liang Zhang <psychelzh@outlook.com>
2021-04-13 21:27:51 +0800

    🐞fix(games): fix path issue
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/search_games.R
+++ R/search_games.R
@@ -19,8 +19,9 @@
 #'   analyzed and its related information.
 #' @export
 search_games <- function(config_where) {
-  stopifnot(fs::file_exists(query_files[["games"]]))
-  tarflow.iquizoo::fetch(query_files[["games"]], config_where) %>%
+  query_path <- fs::path(query_dir, query_files[["games"]])
+  stopifnot(fs::file_exists(query_path))
+  tarflow.iquizoo::fetch(query_path, config_where) %>%
     dplyr::left_join(dataproc.iquizoo::game_info, by = "game_id") %>%
     dplyr::mutate(prep_fun = rlang::syms(.data[["prep_fun_name"]]))
 }
