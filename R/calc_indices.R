101f71c6d155e75bcd19da0300deb61c6c9e04bf
Liang Zhang <psychelzh@outlook.com>
2021-04-14 22:02:15 +0800

    🐞fix: add data validation
    And the docstring is enhanced with more details on the behavior steps.
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/calc_indices.R
+++ R/calc_indices.R
@@ -11,7 +11,23 @@
 #' @export
 calc_indices <- function(data, prep_fun, name_data = "game_data") {
   vars_by <- setdiff(names(data), name_data)
-  data_parsed <- data %>%
+  #' @details
+  #'
+  #' These steps are performed in order:
+  #'
+  #' 1. Remove observations with invalid json string data. If this step produces
+  #' data with no observation, following steps are skipped and `NULL` is
+  #' returned.
+  data_valid <- dplyr::filter(
+    data, purrr::map_lgl(.data[[name_data]], jsonlite::validate)
+  )
+  if (nrow(data_valid) == 0) {
+    return()
+  }
+  #' 1. Parse data stored in json string, convert the names to lower case and
+  #' stack the parsed data. Stacking have better performances than
+  #' [group_nest][dplyr::group_nest()]ing.
+  data_parsed <- data_valid %>%
     dplyr::mutate(
       "{name_data}" := purrr::map(
         .data[[name_data]],
@@ -20,13 +36,15 @@ calc_indices <- function(data, prep_fun, name_data = "game_data") {
       )
     ) %>%
     tidyr::unnest(.data[[name_data]])
+  #' 1. Call [dataproc.iquizoo::preproc_data()] to pre-process on the parsed
+  #' data, and the results are stacked into a long format. The longer format is
+  #' used because each observation has its own creating time information.
   dataproc.iquizoo::preproc_data(
     data_parsed,
     deparse1(substitute(prep_fun)),
     vars_by,
     character.only = TRUE
   ) %>%
-    # results must be stacked for there might be game time
     tidyr::pivot_longer(
       !dplyr::all_of(vars_by),
       names_to = "index",
