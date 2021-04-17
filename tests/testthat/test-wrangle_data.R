8bee97ffcebd4bdba13637c180ff0d65ef6f3933
Liang Zhang <psychelzh@outlook.com>
2021-04-17 23:26:18 +0800

    🦄refactor(defunct): remove calc_indices
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- tests/testthat/test-wrangle_data.R
+++ tests/testthat/test-wrangle_data.R
@@ -7,3 +7,30 @@ test_that("Should return with attributes (meta information):", {
   )
   expect_snapshot(wrangle_data(data))
 })
+
+test_that("Can deal with invalid json (remove it)", {
+  data_case_invalid <- tibble::tibble(
+    id = 1,
+    game_data = jsonlite::toJSON(
+      data.frame(NHit = 10, Feedback = 1)
+    ) %>%
+      substr(1, 5)
+  )
+  expect_silent(be_null <- wrangle_data(data_case_invalid))
+  expect_null(be_null)
+})
+
+test_that("Can deal with empty json (remove it)", {
+  data_case_empty1 <- tibble::tibble(
+    id = 1,
+    game_data = "[]"
+  )
+  data_case_empty2 <- tibble::tibble(
+    id = 1,
+    game_data = "{}"
+  )
+  expect_silent(be_null <- wrangle_data(data_case_empty1))
+  expect_null(be_null)
+  expect_silent(be_null <- wrangle_data(data_case_empty2))
+  expect_null(be_null)
+})
