7b71a3b2e646bed4cebb5073a6e8c5034349e3dd
Liang Zhang <psychelzh@outlook.com>
2021-05-15 14:58:31 +0800

    🐞 fix(tmpl): update queries
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- inst/templates/sql/games.tmpl.sql
+++ inst/templates/sql/games.tmpl.sql
@@ -3,9 +3,9 @@
 SELECT DISTINCT
 	content.Id game_id
 FROM
-	iquizoo_user_db.user_organization
-	INNER JOIN iquizoo_user_db.base_organization ON base_organization.Id = user_organization.OrganizationId  -- `base_organization` might be used in "where_clause"
-	INNER JOIN iquizoo_content_db.project_course_user ON project_course_user.UserId = user_organization.Id
+	iquizoo_user_db.organization_user
+	INNER JOIN iquizoo_user_db.base_organization ON base_organization.Id = organization_user.OrganizationId  -- `base_organization` might be used in "where_clause"
+	INNER JOIN iquizoo_content_db.project_course_user ON project_course_user.UserId = organization_user.Id
 	INNER JOIN iquizoo_content_db.course ON course.Id = project_course_user.CourseId -- `course` might be used in "where_clause"
 	INNER JOIN iquizoo_content_db.course_child ON course_child.CourseId = course.Id
 	INNER JOIN iquizoo_content_db.course_child_config ON course_child_config.ChildCourseId = course_child.Id
--- inst/templates/sql/users.tmpl.sql
+++ inst/templates/sql/users.tmpl.sql
@@ -1,10 +1,10 @@
 -- !preview conn=DBI::dbConnect(odbc::odbc(), "iquizoo-v3", database = "iquizoo_datacenter_db")
 
 SELECT DISTINCT
-	`user`.Id user_id,
-	`user`.RealName user_name,
-	`user`.Gender user_sex,
-	`user`.Birthday user_dob,
+	organization_user.Id user_id,
+	organization_user.RealName user_name,
+	organization_user.Gender user_sex,
+	organization_user.Birthday user_dob,
 	base_organization.`Name` school,
 	base_organization.Province province,
 	base_organization.City city,
@@ -12,11 +12,10 @@ SELECT DISTINCT
 	base_grade_class.GradeName grade,
 	base_grade_class.ClassName class
 FROM
-	iquizoo_user_db.`user`
-	INNER JOIN iquizoo_user_db.user_organization ON user_organization.UserId = `user`.Id
-	INNER JOIN iquizoo_user_db.base_organization ON base_organization.Id = user_organization.OrganizationId  -- `base_organization` might be used in "where_clause"
-	INNER JOIN iquizoo_user_db.base_grade_class ON base_grade_class.Id = user_organization.ClassId
-	INNER JOIN iquizoo_content_db.project_course_user ON project_course_user.UserId = user_organization.Id
+	iquizoo_user_db.organization_user
+	INNER JOIN iquizoo_user_db.base_organization ON base_organization.Id = organization_user.OrganizationId  -- `base_organization` might be used in "where_clause"
+	INNER JOIN iquizoo_user_db.base_grade_class ON base_grade_class.Id = organization_user.ClassId
+	INNER JOIN iquizoo_content_db.project_course_user ON project_course_user.UserId = organization_user.Id
 	INNER JOIN iquizoo_content_db.course ON course.Id = project_course_user.CourseId -- `course` might be used in "where_clause"
 	INNER JOIN iquizoo_content_db.course_child ON course_child.CourseId = course.Id
 	INNER JOIN iquizoo_content_db.course_child_config ON course_child_config.ChildCourseId = course_child.Id
