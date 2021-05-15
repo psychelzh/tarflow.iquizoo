133e8f69727525f33dfdc5db71c2f6fca29b14cf
Liang Zhang <psychelzh@outlook.com>
2021-05-16 00:17:29 +0800

    🐞 fix: use `ability_id`
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- inst/templates/sql/abilities.sql
+++ inst/templates/sql/abilities.sql
@@ -1,8 +1,7 @@
 SELECT
-	content.Id game_id,
+	content_ability.Id ability_id,
 	content_ability.AbilityTypeName ab_type,
 	content_ability.FirstAbilityName ab_name_first,
 	content_ability.SecondAbilityName ab_name_second
 FROM
-	iquizoo_content_db.content
-	INNER JOIN iquizoo_content_db.content_ability ON content.Id = content_ability.ContentId;
+	iquizoo_content_db.content_ability;
