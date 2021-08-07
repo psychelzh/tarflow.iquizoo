3b9fcad83b7caf5ebd89128fb54bb6f928f2952d
Liang Zhang <psychelzh@outlook.com>
2021-08-07 16:27:41 +0800

    🐞 fix(db): new user structure
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- inst/templates/sql/data.tmpl.sql
+++ inst/templates/sql/data.tmpl.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT
-	organization_user.Id user_id,
+	v_organizationuser.Id user_id,
 	course.`Name` course_name,
 	content.Id game_id,
 	content.`Name` game_name,
@@ -8,8 +8,9 @@ SELECT DISTINCT
 	content_orginal_data_detail.OrginalData game_data
 FROM
 	iquizoo_datacenter_db.content_orginal_data_detail
-	INNER JOIN iquizoo_user_db.organization_user ON organization_user.Id = content_orginal_data_detail.OrganizationUserId
-	INNER JOIN iquizoo_user_db.base_organization ON base_organization.Id = organization_user.OrganizationId -- `base_organization` might be used in "where_clause"
+	INNER JOIN iquizoo_content_db.v_organizationuser ON v_organizationuser.OrganizationUserId = content_orginal_data_detail.OrganizationUserId
+	INNER JOIN iquizoo_user_db.base_organization ON base_organization.Id = v_organizationuser.OrganizationId -- `base_organization` might be used in "where_clause"
 	INNER JOIN iquizoo_content_db.content ON content.Id = content_orginal_data_detail.ContentId -- `content` might be used in "where_clause"
-	INNER JOIN iquizoo_content_db.course ON course.Id = content_orginal_data_detail.CourseId -- `course` might be used in "where_clause"
+	INNER JOIN iquizoo_content_db.project_course_config ON project_course_config.Id = content_orginal_data_detail.ProjectCourseConfigId
+	INNER JOIN iquizoo_content_db.course ON course.Id = project_course_config.CourseId -- `course` might be used in "where_clause"
 { where_clause };
--- inst/templates/sql/games.tmpl.sql
+++ inst/templates/sql/games.tmpl.sql
@@ -1,9 +1,9 @@
 SELECT DISTINCT
 	content.Id game_id
 FROM
-	iquizoo_user_db.organization_user
-	INNER JOIN iquizoo_user_db.base_organization ON base_organization.Id = organization_user.OrganizationId  -- `base_organization` might be used in "where_clause"
-	INNER JOIN iquizoo_content_db.project_course_user ON project_course_user.OrganizationUserId = organization_user.Id
+	iquizoo_content_db.v_organizationuser
+	INNER JOIN iquizoo_user_db.base_organization ON base_organization.Id = v_organizationuser.OrganizationId  -- `base_organization` might be used in "where_clause"
+	INNER JOIN iquizoo_content_db.project_course_user ON project_course_user.OrganizationUserId = v_organizationuser.Id
 	INNER JOIN iquizoo_content_db.course ON course.Id = project_course_user.CourseId -- `course` might be used in "where_clause"
 	INNER JOIN iquizoo_content_db.course_child ON course_child.CourseId = course.Id
 	INNER JOIN iquizoo_content_db.course_child_config ON course_child_config.ChildCourseId = course_child.Id
