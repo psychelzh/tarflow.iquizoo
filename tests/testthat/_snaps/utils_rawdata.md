3432869982cceb154fde4bde119fe039ff802f48
Liang Zhang <psychelzh@outlook.com>
2021-11-24 20:38:31 +0800

    test(preproc_data): test complex verbs
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- tests/testthat/_snaps/utils_rawdata.md
+++ tests/testthat/_snaps/utils_rawdata.md
@@ -105,3 +105,32 @@
       2     2          1              1             0
       
 
+# Complex dplyr verbs in `preproc_data()`
+
+    Code
+      dm_indices
+    Output
+      -- Metadata --------------------------------------------------------------------
+      Tables: `meta`, `indices`
+      Columns: 10
+      Primary keys: 2
+      Foreign keys: 1
+
+---
+
+    Code
+      dm::dm_get_tables(dm_indices)
+    Output
+      $meta
+      # A tibble: 1 x 2
+          .id user_id
+        <int>   <dbl>
+      1     1       1
+      
+      $indices
+      # A tibble: 1 x 8
+          .id    nc   mrt  rtsd dprime         c commissions omissions
+        <int> <int> <dbl> <dbl>  <dbl>     <dbl>       <int>     <int>
+      1     1     8 1194.  523. -0.460 -5.55e-17           6         6
+      
+
--- tests/testthat/test-utils_rawdata.R
+++ tests/testthat/test-utils_rawdata.R
@@ -63,9 +63,32 @@ test_that("Basic situation in `preproc_data()`", {
       jsonlite::toJSON(data.frame(nhit = 1, feedback = 1))
     )
   )
+  skip_if_not_installed("preproc.iquizoo", "1.3.0")
+  library(preproc.iquizoo)
   dm_indices <- data |>
     wrangle_data() |>
-    preproc_data(prep_fun_name = bart)
+    preproc_data(prep_fun = bart)
+  expect_snapshot(dm_indices)
+  expect_snapshot(dm::dm_get_tables(dm_indices))
+})
+
+test_that("Complex dplyr verbs in `preproc_data()`", {
+  set.seed(1)
+  data <- tibble::tibble(
+    user_id = 1,
+    game_data = jsonlite::toJSON(
+      data.frame(
+        acc = sample(c(0, 1), 20, replace = TRUE),
+        type = sample(rep(c("left", "right"), 10)),
+        rt = runif(20, 300, 2000)
+      )
+    )
+  )
+  skip_if_not_installed("preproc.iquizoo", "1.3.0")
+  library(preproc.iquizoo)
+  dm_indices <- data |>
+    wrangle_data() |>
+    preproc_data(prep_fun = cpt)
   expect_snapshot(dm_indices)
   expect_snapshot(dm::dm_get_tables(dm_indices))
 })
