40ee2cd1fc206a8d794a8039e51b8681ee13899a
Liang Zhang <psychelzh@outlook.com>
2021-11-25 10:03:55 +0800

    refactor(upkeep): remove deprecated
    fix #42
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/constants.R
+++ R/constants.R
@@ -1,38 +1,3 @@
-#' Supported schemas
-#'
-#' Character vector. Name is used as a short memo. Developers should describe it
-#' in detail in the value because it is displayed in the wizard.
-#'
-#' @keywords internal
-schemas <- c(
-  scores = "Download pre-calculated scores from database",
-  original = "Download original data only",
-  preproc = "Download original data and preprocess it"
-)
-
-#' Configuration file
-#'
-#' @keywords internal
-config_file <- "config.yml"
-
-#' Query files used
-#'
-#' Character vector. Name should be used as the name of the fetched object.
-#'
-#' @keywords internal
-query_files <- c(
-  users = "users.tmpl.sql",
-  scores = "scores.tmpl.sql",
-  data = "data.tmpl.sql",
-  abilities = "abilities.sql",
-  games = "games.tmpl.sql" # typically not in pipeline
-)
-
-#' Query directory name
-#'
-#' @keywords internal
-query_dir <- "sql"
-
 #' Key variables in data
 #'
 #' @keywords internal
