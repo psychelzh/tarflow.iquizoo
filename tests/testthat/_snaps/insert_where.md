f3294e791a33173a6371180c3538b273d579e1ce
Liang Zhang <psychelzh@outlook.com>
2021-11-25 21:37:04 +0800

    test(insert_where): test edge cases
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- tests/testthat/_snaps/insert_where.md
+++ tests/testthat/_snaps/insert_where.md
@@ -2,19 +2,21 @@
 
     [
       {
-        "table": "content"
+        "table": "content",
+        "field": "name",
+        "values": "test_new"
       }
     ]
 
 ---
 
     Code
-      insert_where(config_where, list(table = "content", values = 1))
+      insert_where(config_where, list(table = "content", field = "name", values = "test_new"))
     Output
-      # A tibble: 1 x 2
-        table   values   
-        <chr>   <list>   
-      1 content <chr [1]>
+      # A tibble: 1 x 3
+        table   field values   
+        <chr>   <chr> <list>   
+      1 content name  <chr [1]>
 
 # Check `replace = FALSE`
 
@@ -25,7 +27,29 @@
         "values": "test"
       },
       {
-        "table": "content"
+        "table": "content",
+        "field": "name",
+        "values": "test_new"
       }
     ]
+
+# Works with empty old
+
+    [
+      {
+        "table": "content",
+        "field": "name",
+        "values": "test_new"
+      }
+    ]
+
+---
+
+    Code
+      insert_where(data.frame(), list(table = "content", field = "name", values = "test_new"))
+    Output
+      # A tibble: 1 x 3
+        table   field values   
+        <chr>   <chr> <list>   
+      1 content name  <chr [1]>
 
--- tests/testthat/test-insert_where.R
+++ tests/testthat/test-insert_where.R
@@ -1,21 +1,63 @@
+config_where <- list(
+  list(table = "content", field = "name", values = "test")
+)
 test_that("Check method dispatch works", {
-  config_where <- list(
-    list(table = "content", field = "name", values = "test")
+  expect_snapshot_value(
+    insert_where(
+      config_where,
+      list(
+        table = "content",
+        field = "name",
+        values = "test_new"
+      )
+    )
   )
-  expect_snapshot_value(insert_where(config_where, list(table = "content")))
   config_where <- data.frame(
     table = "content", field = "name", values = "test"
   )
   expect_snapshot(
-    insert_where(config_where, list(table = "content", values = 1))
+    insert_where(
+      config_where, list(
+        table = "content",
+        field = "name",
+        values = "test_new"
+      )
+    )
   )
 })
 
 test_that("Check `replace = FALSE`", {
-  config_where <- list(
-    list(table = "content", field = "name", values = "test")
-  )
   expect_snapshot_value(
-    insert_where(config_where, list(table = "content"), replace = FALSE)
+    insert_where(
+      config_where, list(
+        table = "content",
+        field = "name",
+        values = "test_new"
+      ),
+      replace = FALSE
+    )
+  )
+})
+
+test_that("Works with empty old", {
+  expect_snapshot_value(
+    insert_where(NULL, list(
+      table = "content",
+      field = "name",
+      values = "test_new"
+    ))
+  )
+  expect_snapshot(
+    insert_where(data.frame(), list(
+      table = "content",
+      field = "name",
+      values = "test_new"
+    ))
+  )
+})
+
+test_that("Error for invalid where config", {
+  expect_error(
+    insert_where(config_where, list(table2 = "")), class = "where_invalid"
   )
 })
