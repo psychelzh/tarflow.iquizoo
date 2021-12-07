8947467347bc3f476477eeeedca45b6c051b45df
Liang Zhang <psychelzh@outlook.com>
2021-12-07 11:18:39 +0800

    fix(compat): adapt to new preproc
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/preproc_data.R
+++ R/preproc_data.R
@@ -7,15 +7,17 @@
 #'   two tables: `meta` and `data`.
 #' @param .fn This can be a function or formula. See [rlang::as_function()] for
 #'   more details.
+#' @param ... Additional arguments passed to `.fn`. Note `.by` argument is
+#'   handled by current function itself, do not pass it.
 #' @return A [tibble][tibble::tibble-package] of calculated indices.
 #' @export
-preproc_data <- function(dm, .fn) {
+preproc_data <- function(dm, .fn, ...) {
   .fn <- as_function(.fn)
   .key <- dm::dm_get_all_pks(dm, "data") |>
     purrr::pluck("pk_col", 1)
   dm::reunite_parent_child(
     dm::pull_tbl(dm, "meta"),
-    .fn(dm::pull_tbl(dm, "data"), .key),
+    .fn(dm::pull_tbl(dm, "data"), .key, ...),
     id_column = !!.key
   )
 }
