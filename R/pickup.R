bbc0b572a963584268cfa0cd12be79f891e2194b
Liang Zhang <psychelzh@outlook.com>
2022-03-17 22:12:42 +0800

    fix(pickup): fix `ifelse` issue
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/pickup.R
+++ R/pickup.R
@@ -4,8 +4,9 @@
 #' extracted from a given database.
 #'
 #' @name pickup
-#' @param query_file File name of `sql` query. If not found, will try using it
-#'   as a sql query directly.
+#' @param query_file File name of `sql` query. Literal query is acceptable, and
+#'   to be recognized as literal query, the input must be a string containing at
+#'   least one new line.
 #' @param config_where Configuration of "where-clause" of the `sql` query. Can
 #'   be a `list` (mostly from the `config.yml` file) or `data.frame`.
 #' @param dsn The data source name of an **ODBC** database connector. See
@@ -31,11 +32,11 @@ pickup <- function(query_file,
   )
   on.exit(DBI::dbDisconnect(con))
   query <- ifelse(
-    file.exists(query_file),
-    readLines(query_file, encoding = encoding),
-    query_file
+    stringr::str_detect(query_file, "\\n"),
+    query_file,
+    readLines(query_file, encoding = encoding) |>
+      stringr::str_c(collapse = "\n")
   ) |>
-    stringr::str_c(collapse = "\n") |>
     stringr::str_glue(
       .envir = env(
         where_clause = compose_where(config_where)
