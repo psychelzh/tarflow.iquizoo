ffe40ad0c6e4d685589b93ff271989f7cfcc1007
Liang Zhang <psychelzh@outlook.com>
2022-03-23 21:36:45 +0800

    refactor(preproc): throw errors
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- tests/testthat/test-preproc_data.R
+++ tests/testthat/test-preproc_data.R
@@ -12,20 +12,3 @@ test_that("Basic situation in `preproc_data()`", {
     expect_silent() |>
     expect_snapshot_value(style = "json2")
 })
-
-test_that("Complex dplyr verbs in `preproc_data()`", {
-  withr::local_seed(1)
-  data <- tibble::tibble(
-    user_id = 1,
-    raw_parsed = list(
-      data.frame(
-        acc = sample(c(0, 1), 20, replace = TRUE),
-        type = sample(rep(c("target", "nontarget"), 10)),
-        rt = runif(20, 300, 2000)
-      )
-    )
-  )
-  preproc_data(data, fn = cpt) |>
-    expect_silent() |>
-    expect_snapshot_value(style = "json2")
-})
