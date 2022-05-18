e9b827ac9e956d8fbcb632e68a727fd670b0f242
Liang Zhang <psychelzh@outlook.com>
2022-05-18 10:40:02 +0800

    docs(preproc): correct docstring
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/preproc_data.R
+++ R/preproc_data.R
@@ -1,13 +1,6 @@
 #' Feed Raw Data to Pre-processing
 #'
-#' Integrate data returned by [wrangle_data()] with functions in preproc.iquizoo
-#' package.
-#'
-#' The calculated indices are stored in a [data.frame][data.frame()] of
-#' [longer][tidyr::pivot_longer()] format with two columns named `"index_name"`
-#' and `"score"` (both could be configured) respectively and are nested into a
-#' [tibble][tibble::tibble-package] object. The longer format is used to
-#' facilitate combining.
+#' Calculate indices using data returned by [wrangle_data()].
 #'
 #' @param data A [tibble][tibble::tibble-package] contains raw data.
 #' @param fn This can be a function or formula. See [rlang::as_function()] for
