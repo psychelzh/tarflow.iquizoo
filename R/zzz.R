7d7a398666f4f4d9866184d5ed50b9c598701d06
Liang Zhang <psychelzh@outlook.com>
2023-02-15 22:06:10 +0800

    Make sure options are set correctly when loading
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/zzz.R
+++ R/zzz.R
@@ -12,12 +12,16 @@ NULL
     op_tarflow <- list(
       tarflow.driver = RMariaDB::MariaDB()
     )
-    toset <- !(names(op_tarflow) %in% names(op))
-    if (any(toset)) options(op_tarflow[toset])
   } else {
-    message("Neither odbc nor RMariaDB is installed.",
-            " Please install one of them.")
+    op_tarflow <- list()
+    message(
+      "Neither odbc nor RMariaDB is installed.",
+      " Please install one of them."
+    )
   }
+
+  toset <- !(names(op_tarflow) %in% names(op))
+  if (any(toset)) options(op_tarflow[toset])
 
   invisible()
 }
