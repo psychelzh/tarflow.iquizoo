4e27a2d614286af8786462bb6ef3337c42d97ca4
Liang Zhang <psychelzh@outlook.com>
2023-02-15 23:27:12 +0800

    Move package startup message to .onAttach
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/zzz.R
+++ R/zzz.R
@@ -14,14 +14,18 @@ NULL
     )
   } else {
     op_tarflow <- list()
-    message(
-      "Neither odbc nor RMariaDB is installed.",
-      " Please install one of them."
-    )
   }
 
   toset <- !(names(op_tarflow) %in% names(op))
   if (any(toset)) options(op_tarflow[toset])
 
   invisible()
+}
+
+.onAttach <- function(libname, pkgname) {
+  if (!requireNamespace("odbc", quietly = TRUE) ||
+      !requireNamespace("RMariaDB", quietly = TRUE)) {
+    packageStartupMessage("Neither odbc nor RMariaDB is installed.",
+                          " Please install one of them.")
+  }
 }
