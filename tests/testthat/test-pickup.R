7dde2e407ebcd43bc1eb182f1e0df27ad775eb69
Liang Zhang <psychelzh@outlook.com>
2023-02-16 14:00:00 +0800

    Better testing codes
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- tests/testthat/test-pickup.R
+++ tests/testthat/test-pickup.R
@@ -3,13 +3,11 @@ test_that("`pickup()` smoke testing", {
   mockery::stub(pickup, "DBI::dbDisconnect", TRUE)
   mockery::stub(pickup, "DBI::dbGetQuery", data.frame())
   mockery::stub(pickup, "compose_where", "")
-  mockery::stub(pickup, "odbc::odbc()", structure(0, class = "OdbcDriver"))
-  withr::local_options(tarflow.driver = odbc::odbc())
+  mockery::stub(pickup, "readLines", "{where_clause}")
+  withr::local_options(tarflow.driver = structure(0, class = "OdbcDriver"))
   pickup("A\n{where_clause}") |>
     expect_silent() |>
     expect_equal(tibble::tibble())
-
-  mockery::stub(pickup, "readLines", "{where_clause}")
   pickup("") |>
     expect_silent() |>
     expect_equal(tibble::tibble())
@@ -18,8 +16,8 @@ test_that("`pickup()` smoke testing", {
 test_that("Works for both drivers", {
   mockery::stub(pickup, "DBI::dbConnect", TRUE)
   mockery::stub(pickup, "DBI::dbDisconnect", TRUE)
-  mockery::stub(pickup, "compose_where", "")
   mockery::stub(pickup, "DBI::dbGetQuery", data.frame())
+  mockery::stub(pickup, "compose_where", "")
   mockery::stub(pickup, "readLines", "{where_clause}")
   withr::with_options(
     list(tarflow.driver = structure(0, class = "OdbcDriver")),
