28fd92f3f63003c70e4442a5b20301810498fb39
Liang Zhang <psychelzh@outlook.com>
2023-08-28 20:49:27 +0800

    Use better order
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- inst/sql/fetch_scores_glue.sql
+++ inst/sql/fetch_scores_glue.sql
@@ -2,9 +2,9 @@ SELECT
     ProjectCourseConfigId project_id,
     OrganizationUserId user_id,
     ContentId game_id,
-    ContentAbilityId ability_id,
     ContentVersion game_version,
     CreateTime game_time,
+    ContentAbilityId ability_id,
     ApproximateScore game_score_raw,
     StandardScore game_score_std
 FROM
