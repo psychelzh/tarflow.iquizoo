8431829dc9fae32f471addefa60300337c362bb9
Liang Zhang <psychelzh@outlook.com>
2023-08-29 23:43:19 +0800

    Add stage and star data
    To fix #29
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- inst/sql/fetch_scores_glue.sql
+++ inst/sql/fetch_scores_glue.sql
@@ -5,6 +5,8 @@ SELECT
     ContentVersion game_version,
     CreateTime game_time,
     ContentAbilityId ability_id,
+    Stage game_stage,
+    Star game_star,
     ApproximateScore game_score_raw,
     StandardScore game_score_std
 FROM
