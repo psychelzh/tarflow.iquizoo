9e1217c2920d470c68f7a4bef1f91077eeb623b8
Liang Zhang <psychelzh@outlook.com>
2023-08-30 10:38:53 +0800

    Use better skip logic
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- tests/testthat/test-prepare_fetch_data.R
+++ tests/testthat/test-prepare_fetch_data.R
@@ -1,5 +1,6 @@
 test_that("Smoke test", {
-  skip_on_ci()
+  skip_if_not_installed("odbc")
+  skip_if(!"iquizoo-v3" %in% odbc::odbcListDataSources()$name)
   tbl_params <- tibble::tribble(
     ~course_name, ~course_period,
     "509测试", "高中"
@@ -17,7 +18,8 @@ test_that("Smoke test", {
 })
 
 test_that("Work with `tar_make()", {
-  skip_on_ci()
+  skip_if_not_installed("odbc")
+  skip_if(!"iquizoo-v3" %in% odbc::odbcListDataSources()$name)
   targets::tar_dir({
     targets::tar_script({
       library(targets)
