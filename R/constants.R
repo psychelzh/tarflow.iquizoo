76f1be790632b8772aec1341697d3706c43f5418
Liang Zhang <psychelzh@outlook.com>
2023-08-31 20:44:27 +0800

    Get rid of internal data by constants
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/constants.R
+++ R/constants.R
@@ -7,3 +7,25 @@ name_sql_files <- c(
   raw_data = "fetch_raw_data_glue.sql",
   scores = "fetch_scores_glue.sql"
 )
+course_periods <- data.frame(
+  course_period_code = 0:7,
+  course_period_name = c(
+    "\u672a\u6307\u5b9a",
+    "\u5b66\u524d",
+    "\u5c0f\u5b66\u4f4e\u6bb5",
+    "\u5c0f\u5b66\u4e2d\u6bb5",
+    "\u5c0f\u5b66\u9ad8\u6bb5",
+    "\u5c0f\u5b66",
+    "\u521d\u4e2d",
+    "\u9ad8\u4e2d"
+  )
+)
+game_types <- data.frame(
+  game_type_code = 1:4,
+  game_type_name = c(
+    "\u6d4b\u8bc4\u6e38\u620f",
+    "\u8bad\u7ec3\u6e38\u620f",
+    "\u9898\u76ee\u58f3",
+    "\u8bfe\u7a0b\u89c6\u9891"
+  )
+)
