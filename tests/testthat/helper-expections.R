bce47b55b195946c040a837a55135514e7d2937e
Liang Zhang <psychelzh@outlook.com>
2023-10-21 21:57:36 +0800

    Fix lint
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- tests/testthat/helper-expections.R
+++ tests/testthat/helper-expections.R
@@ -1,11 +1,15 @@
 expect_targets_list <- function(targets) {
-  act <- quasi_label(rlang::enquo(targets), arg = "targets")
+  act <- testthat::quasi_label(rlang::enquo(targets), arg = "targets")
   if (!is.list(act$val)) {
-    fail(sprintf("%s should be a list, not a %s.", act$lab, typeof(act$val)))
+    testthat::fail(
+      sprintf("%s should be a list, not a %s.", act$lab, typeof(act$val))
+    )
   }
   if (!all(purrr::map_lgl(unlist(act$val), \(x) inherits(x, "tar_target")))) {
-    fail(sprintf("All elements of %s should be valid target objects.", act$lab))
+    testthat::fail(
+      sprintf("All elements of %s should be valid target objects.", act$lab)
+    )
   }
-  succeed()
+  testthat::succeed()
   invisible(act$val)
 }
