a7b1861a2ee88298c3c667b81dc9b8501bf46d29
Liang Zhang <psychelzh@outlook.com>
2023-10-26 12:21:50 +0800

    Fix reference error
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- inst/pipelines/use_targets.R
+++ inst/pipelines/use_targets.R
@@ -1,4 +1,4 @@
-# Created by tarflow.iquizoo::use_targets().
+# Created by tarflow.iquizoo::use_targets_pipeline().
 # Follow the comments below to fill in this target script.
 # Then follow the manual to check and run the pipeline:
 #   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline
