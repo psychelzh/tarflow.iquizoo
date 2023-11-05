4603d7c15afb47cf6944c19ae144aac75687f940
Liang Zhang <psychelzh@outlook.com>
2023-11-05 16:35:34 +0800

    Separate `use_targets_pipeline()`
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/targets.R
+++ R/targets.R
@@ -1,39 +1,3 @@
-# nocov start
-
-#' Create standard data fetching targets pipeline script
-#'
-#' This function creates a standard data fetching targets pipeline script
-#' for you to fill in.
-#'
-#' @return NULL (invisible). This function is called for its side effects.
-#' @export
-use_targets_pipeline <- function() {
-  script <- "_targets.R"
-  if (file.exists(script)) {
-    cli::cli_alert_info(
-      sprintf("File {.file %s} exists. Stash and retry.", script)
-    )
-    return(invisible())
-  }
-  copy_success <- file.copy(
-    system.file(
-      "pipelines", "use_targets.R",
-      package = "tarflow.iquizoo"
-    ),
-    script
-  )
-  if (!copy_success) {
-    cli::cli_alert_danger("Sorry, copy template failed.")
-    return(invisible())
-  }
-  cli::cli_alert_success(
-    sprintf("File {.file %s} created successfully.", script)
-  )
-  return(invisible())
-}
-
-# nocov end
-
 #' Generate a set of targets for pre-processing of iQuizoo data
 #'
 #' This target factory prepares a set of target objects used to fetch data from
