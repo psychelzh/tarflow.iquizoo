a833955f7abae245d52fc0912911c056d1f5b703
Liang Zhang <psychelzh@outlook.com>
2024-10-29 09:50:30 +0800

    Add one test case

--- tests/testthat/test_basic-workflow.R
+++ tests/testthat/test_basic-workflow.R
@@ -17,3 +17,26 @@ targets::tar_test("Workflow works", {
   expect_snapshot_value(targets::tar_read(users), style = "json2")
   expect_snapshot_value(targets::tar_read(scores), style = "json2")
 })
+
+targets::tar_test("Users properties fetching correctly", {
+  skip_if_not_installed("preproc.iquizoo")
+  targets::tar_script({
+    params <- tibble::tribble(
+      ~organization_name, ~project_name, ~course_name, ~game_name,
+      NA, "北师大附属实验中学大脑课堂体验课前测（2023级初一）", NA, NA
+    )
+    contents <- fetch_iquizoo(
+      read_file(setup_templates()$contents),
+      params = unname(as.list(params))
+    )[1, ]
+    tarflow.iquizoo::tar_prep_iquizoo(
+      contents = contents,
+      what = "scores",
+      combine = "scores",
+      cache = cachem::cache_mem()
+    )
+  })
+  targets::tar_make(reporter = "silent", callr_function = NULL)
+  expect_snapshot_value(targets::tar_objects(), style = "json2")
+  expect_snapshot_value(targets::tar_read(users), style = "json2")
+})
